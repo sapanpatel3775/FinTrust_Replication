4f6d3b6f24ba9c1fea939114ca32156af57c9351
akaashdash <akaash.dash@gmail.com>
2024-04-16 17:47:20 -0400

    fix our transitive consistency script

--- Generate_Consistency/generate_tra_consis.py
+++ Generate_Consistency/generate_tra_consis.py
@@ -4,18 +4,18 @@ import pandas as pd
 def make_tra(data, constituents_data, top_data):
     for i in data:
         for j in constituents_data:
-            if j[1] == i[2]:
+            if j[1] == i[1]:
                 sector = j[2]
                 break
         for j in top_data:
             if j[0] == sector:
                 top_c = j[1]
                 top_c_ = j[2]
-                if j[1] == i[2]:
+                if j[1] == i[1]:
                     top_c = j[2]
                     top_c_ = j[3]
-        text = i[3].replace(top_c,top_c_).lower().replace(' we ',' '+top_c+' ').replace(' our ',' '+top_c+"'s ").lower()
-        i[3] = text
+        text = i[2].replace(top_c,top_c_).lower().replace(' we ',' '+top_c+' ').replace(' our ',' '+top_c+"'s ").lower()
+        i[2] = text
     return data
 
 def main():
@@ -35,15 +35,5 @@ def main():
     np.save(out_file, data)
     return
 
-def workaround():
-    in_file = '../Data/tra_earnings_call_3days.npy'
-    out_file = '../Used_Data/tra_earnings_call.npy'
-
-    data = np.load(in_file)
-    data = data[:, [0, 2, 3]]
-    print(data[:, [0, 1]])
-
-    np.save(out_file, data)
-
 if __name__ == "__main__":
-    workaround()
+    main()
